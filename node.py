1bfc302d4e2e08a1ac72b9e814d61828825cdfe7
Mahta Rafiee <m77rafiee@yahoo.com>
2019-06-09 20:30:28 +0430

    node.py bug fix

--- node.py
+++ node.py
@@ -18,22 +18,22 @@ class Node:
 		for line in lines[1:]:
 			ip, port, vaddr_self, vaddr_neighbor = line.split()
 			self.neighbors[vaddr_neighbor] = vaddr_self
-			self.link_layer.add_interface(ip, port, vaddr_self, vaddr_neighbor)
+			self.link_layer.add_interface(ip, port, vaddr_neighbor)
 
 		self.distance_vector = {saddr:{saddr:0} for saddr in list(self.neighbors.values())}
 		self.dv_lock = threading.Lock()
 
-	def send_DV_to_neighbors():
+	def send_DV_to_neighbors(self):
 		while(True):
 			time.sleep(1)
 			self.dv_lock.acquire()
 			distance_vector = self.distance_vector
 			self.dv_lock.release()
 			for naddr in self.neighbors:
-				packet = IPpacket(self.neighbors[naddr] ,naddr, CONTROL, distance_vector)
+				packet = IPPacket(self.neighbors[naddr] ,naddr, CONTROL, distance_vector)
 				self.link_layer.send(naddr, packet)
 
-	
+
 
 
 def main():
@@ -42,7 +42,7 @@ def main():
 	functions = [node.send_DV_to_neighbors]
 	threads = []
 	for f in functions:
-		new_thread = threading.Thread(target = f, args = (node,))
+		new_thread = threading.Thread(target = f)
 		threads.append(new_thread)
 		new_thread.start()
 
